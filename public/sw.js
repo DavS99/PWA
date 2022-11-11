c8e68e06baced40d8682bfc26ed80d85be9592cf
DavS99 <davit@ucraft.com>
2022-11-11 16:47:21 +0400

    feat: config push notification

---
+++ public/sw.js
@@ -0,0 +1,37 @@
+let cacheData = 'appV1';
+
+this.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheData).then((cache) => {
+            cache.addAll([
+                '/static/js/main.chunk.js',
+                '/static/js/0.chunk.js',
+                '/static/js/bundle.js',
+                '/index.html',
+                '/'
+            ])
+        })
+    )
+})
+
+this.addEventListener('fetch', (event) => {
+    if (!navigator.onLine) {
+        if (event.request.url === 'http://localhost:3000/static/js/main.chunk.js') {
+            event.waitUntil(
+                this.registration.showNotification('Hello', {
+                    body: 'Hello David Jan'
+                })
+            )
+        }
+        event.respondWith(
+            caches.match(event.request).then((resp) => {
+                if (resp) {
+                    return resp
+                }
+
+                let requestUrl = event.request.clone();
+                fetch(requestUrl)
+            })
+        )
+    }
+})
---
+++ src/swDev.js
@@ -0,0 +1,33 @@
+export default function swDev() {
+    function determineAppServerKey() {
+        const vapidPublicKey = 'BGtkbcjrO12YMoDuq2sCQeHlu47uPx3SHTgFKZFYiBW8Qr0D9vgyZSZPdw6_4ZFEI9Snk1VEAj2qTYI1I1YxBXE';
+        return urlBase64ToUint8Array(vapidPublicKey)
+    }
+
+    function urlBase64ToUint8Array(base64String) {
+        const padding = '='.repeat((4 - base64String.length % 4) % 4);
+        const base64 = (base64String + padding)
+            .replace(/\-/g, '+')
+            .replace(/_/g, '/');
+
+        const rawData = window.atob(base64);
+        const outputArray = new Uint8Array(rawData.length);
+
+        for (let i = 0; i < rawData.length; ++i) {
+            outputArray[i] = rawData.charCodeAt(i);
+        }
+        return outputArray;
+    }
+
+    let swUrl = `${process.env.PUBLIC_URL}/sw.js`;
+    navigator.serviceWorker.register(swUrl).then((response) => {
+        console.log('response', response)
+        return response.pushManager.getSubscription()
+            .then((subscription) => {
+                return response.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: determineAppServerKey()
+                })
+            })
+    })
+}
